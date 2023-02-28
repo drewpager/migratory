b523683fb3f7f2adc9ad5ce7b511dff7ffa2746a
drewpager <drewpage@shredlights.com>
2023-02-28 09:39:46 -0800

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import './App.css';
 import lottie from 'lottie-web';
 
-function App() {
+export const App = () => {
   const contain = useRef(null);
   useEffect(() => {
     lottie.loadAnimation({
@@ -16,9 +16,7 @@ function App() {
   return (
     <div className="App">
       <h1>Siege Media</h1>
-      <div className="contain" ref={contain}></div>
+      <div className="contain" ref={contain} />
     </div>
   );
 }
-
-export default App;
